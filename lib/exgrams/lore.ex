31acc5b673170e16982e0d6f089dd0b501531403
Fernando Schuindt <f.schuindtcs@gmail.com>
2018-11-20 21:33:00 -0200

    feat: Add a little benchmark reporter just for fun

---
+++ lib/exgrams/lore.ex
@@ -0,0 +1,62 @@
+defmodule EXgrams.Lore do
+  @moduledoc """
+  Just a lorem ipsum to be used in performance benchmarks.
+  """
+
+  @spec sentence() :: String.t()
+  def sentence do
+    """
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis nec
+    tellus ac mi tristique semper. Mauris luctus, velit ut lacinia
+    tincidunt, libero elit dictum nunc, in aliquet nibh nisl ut arcu.
+    Maecenas quis eleifend nisl, in sollicitudin felis. Sed quis
+    egestas quam, at ornare diam. Proin tempus lorem eget lectus
+    accumsan vehicula. Sed vel euismod enim. Duis sit amet interdum
+    nunc, laoreet ornare nunc. Sed porttitor nec purus eu interdum.
+    Phasellus congue in ex at sodales. Nullam pellentesque hendrerit
+    congue. Fusce aliquet urna vitae pretium feugiat. Maecenas ac
+    commodo elit. Ut rutrum accumsan nisi. Maecenas id diam quis
+    neque luctus iaculis in et mauris. Integer ac nunc venenatis,
+    faucibus odio in, commodo tortor. Fusce vitae turpis vitae neque
+    euismod sagittis.
+
+    Nullam libero mauris, suscipit vel sagittis nec, maximus sed
+    turpis. Praesent tempor eleifend nunc eget lacinia. Nunc ac
+    ipsum ut lorem elementum mollis eu at massa. Phasellus quis
+    sollicitudin mauris. Mauris sollicitudin justo ac nulla sagittis
+    mattis. Sed varius mauris id ante pretium, et semper nisi 
+    egestas. Cras lobortis pretium nisl id vulputate. Integer nec
+    magna nisl. Ut viverra pretium ligula non dapibus.
+
+    Morbi eget justo ultricies, elementum leo sit amet, accumsan
+    quam. Nunc nisl nulla, mattis eu metus id, lacinia dapibus nisl.
+    In arcu nisl, semper sed scelerisque nec, laoreet nec sapien.
+    Duis sed nunc at dolor dapibus eleifend id at risus. Vivamus
+    mattis tortor in vestibulum rhoncus. Ut in arcu nisl. Vivamus
+    fermentum ut enim nec rutrum. Praesent convallis eros leo,
+    consequat tempor urna pulvinar ac. Praesent a nisl at dolor
+    mattis commodo. Nam auctor lectus quis velit accumsan accumsan.
+
+    Morbi hendrerit ex sed orci viverra, in posuere est luctus.
+    Mauris ullamcorper a nibh a sagittis. Maecenas bibendum
+    consectetur magna tincidunt varius. Pellentesque vel laoreet
+    augue. Vivamus maximus tempor nisi in vulputate. Nulla facilisi.
+    Quisque pellentesque imperdiet lorem, quis elementum erat
+    bibendum sed. Cras cursus mi ut sapien ultricies venenatis.
+    Quisque porttitor diam nec scelerisque luctus. Praesent ut leo
+    dui. Pellentesque interdum dolor in lectus tempor, vel pharetra
+    est pulvinar.
+
+    Vestibulum ante ipsum primis in faucibus orci luctus et ultrices
+    posuere cubilia Curae; Donec facilisis sodales ultrices. Donec
+    scelerisque mauris massa, in dapibus nisi commodo eget. Nullam
+    consequat lacus mi. Phasellus vel velit in est maximus cursus a
+    at magna. Vestibulum non nisl semper, sodales dolor quis,
+    pulvinar erat. Cras bibendum faucibus consequat. Vivamus nec
+    sapien sed massa finibus fermentum. Donec non facilisis nisl.
+    Suspendisse at ante elementum, pharetra orci eget, tincidunt
+    nisi. Integer et rutrum lorem. Aenean facilisis enim eget mollis
+    ullamcorper. Phasellus sagittis venenatis ultricies.
+    """
+  end
+end
---
+++ lib/exgrams/time_frame.ex
@@ -0,0 +1,25 @@
+defmodule EXgrams.TimeFrame do
+  @moduledoc """
+  Calculates the execution time of a function call. Used for
+  benchmarking.
+
+  Taken from:
+  https://medium.com/elixirlabs/implement-a-basic-block-yield-with-elixir-d00f313831f7
+
+  All credits from this module goes to
+  [Mustafa Turan](https://github.com/mustafaturan).
+
+  Just changed the default unit from `:micro_seconds` to
+  `:milliseconds`.
+  """
+
+  defmacro execute(name, units \\ :milliseconds, do: yield) do
+    quote do
+      start = System.monotonic_time(unquote(units))
+      result = unquote(yield)
+      time_spent = System.monotonic_time(unquote(units)) - start
+      IO.puts("Executed #{unquote(name)} in #{time_spent} #{unquote(units)}")
+      result
+    end
+  end
+end
